0a2be04e039b621bacbeb1d8e58846db32aee2d0
trian damai <triannurizkillah@gmail.com>
2024-09-03 16:00:13 +0700

    chore: update some autocomplete

--- components/service/ItemCategory.vue
+++ components/service/ItemCategory.vue
@@ -14,8 +14,8 @@ defineEmits(['select'])
 <template>
   <div class="w-full h-[75vh] sm:w-[50vw] sm:h-[72vh] md:w-[40vw] md:h-[72vh] lg:w-[30vw] lg:h-[72vh] xl:w-[20vw] xl:h-[72vh] mx-4 my-4 flex flex-row justify-center">
     <div class="relative w-full">
-      <div class="absolute bottom-0 flex flex-row justify-center  w-full h-[70vh] sm:w-[50vw] sm:h-[70vh] md:w-[40vw] md:h-[70vh] lg:w-[30vw] lg:h-[70vh] xl:w-[20vw] xl:h-[70vh] px-2">
-        <div :class="{'primary':primary, 'normal':!primary}" class=" w-full h-[70vh]  sm:w-full sm:h-[70vh]  md:w-full md:h-[70vh]  lg:w-full lg:h-[70vh]  xl:w-full xl:h-[75vh] flex flex-row justify-center rounded-2xl border border-gray-300">
+      <div class="absolute bottom-0 flex flex-row justify-center  w-full h-[74vh] sm:w-[50vw] sm:h-[70vh] md:w-[40vw] md:h-[70vh] lg:w-[30vw] lg:h-[70vh] xl:w-[20vw] xl:h-[70vh] px-2">
+        <div :class="{'primary':primary, 'normal':!primary}" class=" w-full h-[75vh] sm:w-full sm:h-[70vh]  md:w-full md:h-[70vh]  lg:w-full lg:h-[70vh]  xl:w-full xl:h-[75vh] flex flex-row justify-center rounded-2xl border border-gray-300">
           <div class="w-full h-full px-12 py-6 flex flex-col justify-between">
             <div class="w-full h-full">
               <div class="h-1/2 flex flex-col justify-between">
@@ -33,7 +33,7 @@ defineEmits(['select'])
                     <span class="rounded-full w-[20px] h-[20px] flex flex-row justify-center">
                       <IconCheckRoundedFilled class="w-full h-full text-blue-800"/>
                     </span>
-                    <span :class="{'text-white':primary}" class="ml-2 text-lg sm:text-sm md:text-sm lg:text-lg xl:text-lg">{{ item }}</span>
+                    <span :class="{'text-white':primary}" class="ml-2 text-sm sm:text-sm md:text-sm lg:text-lg xl:text-lg">{{ item }}</span>
                   </div>
                 </div>
               </div>
@@ -44,7 +44,7 @@ defineEmits(['select'])
           </div>
         </div>
       </div>
-      <div v-show="primary" class="absolute w-full sm:w-[50vw] md:w-[40vw] lg:w-[30vw] xl:w-[20vw] top-4 sm:top-0 md:top-0 lg:top-0 xl:top-0 flex flex-row justify-between">
+      <div v-show="primary" class="absolute w-full sm:w-[50vw] md:w-[40vw] lg:w-[30vw] xl:w-[20vw] top-0 sm:top-0 md:top-0 lg:top-0 xl:top-0 flex flex-row justify-between">
         <span class="star-left"/>
         <span class="bg-yellow-400 text-gray-600 font-semibold rounded-lg w-[60vw] h-[5vh] sm:w-full md:w-full lg:w-[60vw] xl:w-[60vw] text-center place-content-center">Rekomendasi kami</span>
         <span class="star-right"/>
